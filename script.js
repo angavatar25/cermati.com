be5b1a5b01808ada1af255c9298aafef8bf1369d
Ananda Afryan Gemilang <anandalark007@gmail.com>
2020-09-20 18:48:26 +0700

    interval update
    added interval on newsletter

--- script.js
+++ script.js
@@ -6,36 +6,40 @@ $(document).ready(function() {
     
     $('#closeButton').click(function() {
         $('.newsletter__panel').removeClass('appear')
-        $('.newsletter__panel').toggleClass('disappear')
         localStorage.setItem('hiddenNavigation', 'disappear');
+        var toggleHidden = localStorage.getItem('hiddenNavigation');
+        $('.newsletter__panel').toggleClass(toggleHidden)
     })
 
-    //keeping the component not showing up in local storage
-    // $('newsletter__panel').addClass(localStorage.ClassName);
     var currentScrollTop = 0, previousScrollDir = true;
     $(window).scroll(function() {
         var scrollTop = $(this).scrollTop() > 50;
+        var toggleHidden = localStorage.getItem('hiddenNavigation');
+
+        function showNewsletterPanel() {
+            $('.newsletter__panel').addClass('appear');
+            $('.newsletter__panel').removeClass('disappear');
+            localStorage.removeItem('hiddenNavigation');
+        }
+
+        setInterval(showNewsletterPanel, 600000);
+
+        if(toggleHidden == 'disappear') {
+            $('.newsletter__panel').css('display', 'none');
+            $('.newsletter__panel').addClass('disappear');
+        }
+
         if (scrollTop < currentScrollTop) {
             if (!previousScrollDir) {
-              console.log('scrolled up');
               previousScrollDir = true;
             }
         } else {
             if (previousScrollDir) {
-              console.log('scrolled down');
               previousScrollDir = false;
               $('.newsletter__panel').addClass('appear');
             }
         }
         currentScrollTop = scrollTop;
-        // if($(this).scrollTop() > 50) {
-        //     $('.newsletter__panel').addClass('appear');
-        // }
-
-        // else if ($(this).scrollTop() > 50 && localStorage.getItem('hiddenNavigation') == 'disappear') {
-        //     $('.newsletter__panel').toggleClass('disappear');
-        //     $('.newsletter__panel').removeClass('appear');
-        // }
     })
 
     $(window).on('beforeunload', function() {
